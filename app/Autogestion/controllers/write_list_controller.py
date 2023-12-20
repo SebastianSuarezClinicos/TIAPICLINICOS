efbba4b6abedfe77c3a7cef6191a2e258c05d0ff
SuarezSebastian2 <suarezsebastian98@hotmail.com>
2023-12-19 22:12:49 -0500

    Se agrega el update_list para actualizar el ingreso

--- app/Autogestion/controllers/write_list_controller.py
+++ app/Autogestion/controllers/write_list_controller.py
@@ -7,47 +7,79 @@ Created on Mon Dec 19 2023
 
 from decouple import config
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from db.client_graph import get_access_token
 
 
+# Environment variables
+site = config('SITE_AGENDAMIENTO_ECOPETROL')
+sub_site = config('SUBSITE_MEGAS_BULEVAR')
+list_id1 = config('LIST_AUTOAGENDAMIENTO_GESTION_ECOPETROL')
 
 async def write_list(access_token, correo, nombres, apellidos, tipo_identificacion, numero_identificacion, ingreso):
     try:
-        fecha_hora_actual = datetime.now().strftime("%d/%m/%Y %H:%M") #
+        fecha_hora_actual = datetime.utcnow().isoformat()+"Z"
+        numero_identificacion = int(numero_identificacion)
         token = await get_access_token()
-        # Environment variables
-        site = config('SITE_AGENDAMIENTO_ECOPETROL')
-        sub_site = config('SUBSITE_MEGAS_BULEVAR')
-        list_id1 = "1e2054e0-650c-4a95-8203-0ae78b6ee2af"#config('LIST_AUTOAGENDAMIENTO_CITAS_ECOPETROL')
-        print(list_id1)
+
+        #print(list_id1)
         endpoint_url =f'https://graph.microsoft.com/v1.0/sites/{site}/sites/{sub_site}/lists/{list_id1}/items'
         headers = {"Authorization": f"Bearer {token}"}
 
         # Datos a enviar en la solicitud
         item_data = {
-
-                "FechaHoraActual": fecha_hora_actual,
-                "Correo": correo,
+            "fields": {
+                "Fecha": fecha_hora_actual,
+                "Login": correo,
                 "Nombres": nombres,
-                "Apellidos": apellidos,
+                "Apellido": apellidos,
                 "TipoIdentificacion": tipo_identificacion,
-                "NumeroIdentificacion": numero_identificacion,
+                "NoIdentificacion": numero_identificacion,
                 "Ingreso": ingreso
-
+            }
         }
         #return item_data, site, sub_site, list_id, access_token, token
 
         response = requests.post(endpoint_url, json=item_data, headers=headers)
 
         if response.status_code == 201:
-            new_item_id = response.json().get("id")
-            print("Datos escritos en SharePoint con éxito. ID del registro:", new_item_id, item_data)
-            return ("Datos escritos en SharePoint con éxito. ID del registro:", new_item_id, item_data)
+            item_id = response.json().get("id")
+            #print("Datos escritos en SharePoint con éxito. ID del registro:", new_item_id, item_data)
+            return (ingreso, item_id)
         else:
             print("Error al escribir en SharePoint. Código de estado:", response.status_code)
-            return ("Error al escribir en SharePoint. Código de estado:", response.status_code, response.text, list_id1)
+            return ("Error al escribir en SharePoint. Código de estado:", response.status_code, response.text,)
+
+    except Exception as e:
+        print("Error durante la operación:", e)
+        return ("Error durante la operación:", e)
+
+item_id = "2"
+async def uptade_ingreso(ingreso, item_id):
+    try:
+        token = await get_access_token()
+
+        endpoint_url =f'https://graph.microsoft.com/v1.0/sites/{site}/sites/{sub_site}/lists/{list_id1}/items'
+        headers = {"Authorization": f"Bearer {token}"}
+
+        # Datos a enviar en la solicitud
+        item_data = {
+            "fields": {
+                "Ingreso": ingreso
+            }
+        }
+        #return item_data, site, sub_site, list_id, access_token, token
+
+        response = requests.patch(endpoint_url, json=item_data, headers=headers)
+
+        if response.status_code == 201:
+            new_item_id = response.json().get("id")
+            #print("Datos escritos en SharePoint con éxito. ID del registro:", new_item_id, item_data)
+            return (new_item_id)
+        else:
+            print("Error al escribir en SharePoint. Código de estado:", response.status_code)
+            return ("Error al escribir en SharePoint. Código de estado:", response.status_code, response.text,)
 
     except Exception as e:
         print("Error durante la operación:", e)
