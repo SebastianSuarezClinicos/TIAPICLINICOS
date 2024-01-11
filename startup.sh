7aa13a03b0116a1c33ad88cec6fa155cb7040ea1
SebastianSuarezClinicos <sasuarez@clinicos.com.co>
2024-01-10 22:03:51 -0500

    Cambios en startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python3 -m venv entorno
 source entorno/bin/activate
 pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 main:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
