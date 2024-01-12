80aaa16b92344e9de9f360d7c538e349e40d92b6
SebastianSuarezClinicos <sasuarez@clinicos.com.co>
2024-01-12 16:39:05 -0500

    Pruebas de cookies

--- app/Autogestion/routers/Prueba_Cookies_router.py
+++ app/Autogestion/routers/Prueba_Cookies_router.py
@@ -1,13 +1,23 @@
 
 
 from fastapi import APIRouter, Header, Response
+from app.Autogestion.controllers.verification_controller import verify_code
+
+from app.Autogestion.models.verification_model import VerificationModel
 
 
 router = APIRouter(prefix='/Autogestion')
 
 @router.post('/TestCookie', summary="Enviar código de verificación", response_description="Resultado del envío")
-async def sHello(response: Response,
+async def sHello(
+    verification_data: VerificationModel,
+    response: Response,
     authorization: str = Header(None)):
     hello = "Hello World"
+
+    verify_code_response = await verify_code(verification_data, authorization)
+    # Establecer la cookie con el token JWT
+    response.set_cookie(key="accessToken", value=verify_code_response["token"], httponly=True, secure=True, samesite='None', max_age=1800, domain=None)
+
     response.set_cookie(key="hello", value=hello, httponly=True, secure=True, samesite='None', max_age=1800, domain=None)
-    return hello, authorization
+    return ("Verificacion exitosa", verify_code_response["history_result"]), hello, authorization
