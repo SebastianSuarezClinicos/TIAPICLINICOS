f1e9fca3e67397502b1c4d3df6278ef571ca6bea
SebastianSuarezClinicos <sasuarez@clinicos.com.co>
2024-01-12 14:18:05 -0500

    Cambios en funcion de la prueba de cookies

--- app/Autogestion/routers/Prueba_Cookies_router.py
+++ app/Autogestion/routers/Prueba_Cookies_router.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Response
 
 router = APIRouter(prefix='/Autogestion')
 
-@router.post('/send-code', summary="Enviar código de verificación", response_description="Resultado del envío")
+@router.post('/TestCookie', summary="Enviar código de verificación", response_description="Resultado del envío")
 async def sHello(response: Response):
     hello = "Hello World"
     response.set_cookie(key="hello", value=hello, httponly=True, secure=True, samesite='None', max_age=1800, domain=None)
